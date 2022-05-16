8d8dde8239f23ae382c3d36b2346af010b1a4b6d
Azkieel <red.ezequiel@gmail.com>
2022-05-16 23:38:34 +0800

    clean the program for submission

--- Bubble_Sort.rb
+++ Bubble_Sort.rb
@@ -2,11 +2,10 @@ def bubble_sort(array)
   i = 0
   while i < array.length - 1
     array.each_with_index do |current_element, index|
+      next_element = array[index + 1]
       if current_element == array.last()
         array
-      elsif current_element > array[index + 1]  
-        next_element = array[index + 1]
-
+      elsif current_element > next_element
         array[index] = next_element
         array[index + 1] = current_element
       end
